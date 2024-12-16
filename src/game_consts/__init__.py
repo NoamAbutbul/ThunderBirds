3a778e408c8472141a0be04b4b3e8b4694780fc8
NoamAbutbul <135932038+NoamAbutbul@users.noreply.github.com>
2024-12-16 19:43:31 +0200

    update __init__ file

--- src/game_consts/__init__.py
+++ src/game_consts/__init__.py
@@ -1,1 +1,6 @@
 """This file is made for making game_rules as a package. """
+from src.game_consts.load_consts import GAME_NAME, SHORT_SLEEP, FPS, LONG_SLEEP, RULES
+from src.game_consts.load_consts import HEARTS, TIME, LEVEL_FILE_EXTENSION, RECORD_FILE_EXTENSION
+from src.game_consts.load_consts import LEVELS_DIR_PATH, RECORDS_DIR_PATH
+from src.game_consts.load_consts import PORTAL_SIZE, BIG_SHIP_SIZE, SMALL_SHIP_SIZE
+from src.game_consts.load_consts import ALL_COLORS, GAME_SPEED, SPEED_STEPS, MAX_RECORDS_FILES
