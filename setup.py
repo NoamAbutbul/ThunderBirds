9f82ee18982aa8775a142737563d46f60a45782a
NoamAbutbul <135932038+NoamAbutbul@users.noreply.github.com>
2024-12-16 18:49:58 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,7 @@
+"""
+    File to define setup for the project.
+"""
+
+from setuptools import setup, find_packages  # type: ignore
+
+setup(name='setup_ThunderBirds', version='1.0', packages=find_packages())
